3759af70d1c0b6fe25c72cfa1ad0f0f1e3fb41c8
Juan Cardona <juanscr24@hotmail.com>
2025-09-18 12:45:17 -0500

    update

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,23 +1,30 @@
 import { Link } from 'react-router-dom'
 import type { INavbarArray } from './INavbar'
+import netflixLogo from '../../assets/imgs/Netflix.svg'
 
 export const NAVBAR: INavbarArray[] = [
-    { to: '/', name: 'Home' },
     { to: 'movies', name: 'Movies' },
     { to: 'series', name: 'Series' },
 ]
 
 export const Navbar = () => {
     return (
-        <nav className='p-6 bg-amber-300 flex justify-center gap-5'>
-            {NAVBAR.map((item, index) => (
-                <Link
-                    className='text-gray-900 font-bold hover:text-gray-600'
-                    key={index}
-                    to={item.to}>
-                    {item.name}
-                </Link>
-            ))}
+        <nav className='p-7 bg-gray-900 flex justify-around gap-5 text-white items-center'>
+            <Link
+                to={'/'}
+            >
+                <img src={netflixLogo} alt="Netflix" width={90} />
+            </Link>
+            <div className='flex gap-10'>
+                {NAVBAR.map((item, index) => (
+                    <Link
+                        className='text-white font-bold hover:text-gray-600'
+                        key={index}
+                        to={item.to}>
+                        {item.name}
+                    </Link>
+                ))}
+            </div>
         </nav>
     )
 }
